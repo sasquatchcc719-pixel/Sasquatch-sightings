9801db21bbeedaf5c7d72bbf3b04073af05dbfab
Charles Sewell <sasquatchcc719@gmail.com>
2026-02-02 19:17:09 -0700

    Fix SIP domain: add us1 region to Twilio SIP endpoints

--- src/app/api/twilio/call-router/route.ts
+++ src/app/api/twilio/call-router/route.ts
@@ -35,8 +35,8 @@ export async function POST(request: NextRequest) {
       twimlResponse = `<?xml version="1.0" encoding="UTF-8"?>
 <Response>
   <Dial timeout="20" action="https://sasquatchsightings.com/api/twilio/call-after-hours">
-    <Sip>sip:chuck@sasquatch-cc.sip.twilio.com</Sip>
-    <Sip>sip:wife@sasquatch-cc.sip.twilio.com</Sip>
+    <Sip>sip:chuck@sasquatch-cc.sip.us1.twilio.com</Sip>
+    <Sip>sip:wife@sasquatch-cc.sip.us1.twilio.com</Sip>
   </Dial>
 </Response>`
     } else {
